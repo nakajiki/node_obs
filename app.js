ef527a73b28b55164934316a21a54b3fa1421b65
Kazuki Nakajima <nakajima.k0301@gmail.com>
2019-05-21 18:10:42 +0900

    node.jsの非同期処理でハマった
    おそらくPromiseとかasync/awaitについて学ぶ必要がある

--- app.js
+++ app.js
@@ -6,32 +6,40 @@ const mysql = require('mysql')
 const terminalLink = require('terminal-link')
 
 // Database
-const conn = mysql.createConnection({
-    host: process.env.MYSQL_DB_HOST,
-    user: process.env.MYSQL_DB_USERNAME,
-    password: process.env.MYSQL_DB_PASSWORD,
-    port: process.env.MYSQL_DB_PORT,
-    database: process.env.APP_NAME
-})
+async query(sql) {
+    const conn = mysql.createConnection({
+        host: process.env.MYSQL_DB_HOST,
+        user: process.env.MYSQL_DB_USERNAME,
+        password: process.env.MYSQL_DB_PASSWORD,
+        port: process.env.MYSQL_DB_PORT,
+        database: process.env.APP_NAME
+    })
 
-conn.connect((err) => {
-    if (err) {
-        console.error('error connectiong: ' + err.stack)
-        return null
-    }
-    console.info('connected as id ' + conn.threadId)
-    return conn
-})
+    conn.connect((err) => {
+        if (err) {
+            console.error('error connectiong: ' + err.stack)
+            return null
+        }
+        console.info('connected as id ' + conn.threadId)
+        return conn
+    })
 
-conn.query('SELECT * FROM sites', (error, results) => {
-    // Web
-    const app = express()
-    const port = 3000
-    const link = terminalLink('Link:', `http://localhost:${port}`)
+    conn.query(sql, (error, rest) => {
+        console.log(rest)
+        return rest
+    })
 
-    app.get('/', (req, res) => res.send(results))
-    app.listen(port, () => console.log(`App listening on port ${port}\n${link}`))
-})
+    conn.end()
+}
 
-conn.end()
+let sql = 'SELECT * FROM sites'
+let sites = query(sql)
+console.log(sites)
 
+// Web
+const app = express()
+const port = 3000
+const link = terminalLink('Link:', `http://localhost:${port}`)
+
+app.get('/', (req, res) => res.send(sites))
+app.listen(port, () => console.log(`App listening on port ${port}\n${link}`))
